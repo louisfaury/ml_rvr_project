7cee1af3644ff585582b3443824503265f2bc47a
gregallois <gregoire.g-m@hotmail.fr>
2017-04-15 12:13:26 +0200

    repo setup

---
+++ src/distSquared.m
@@ -0,0 +1,13 @@
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Support function to compute basis
+%
+function D2 = distSquared(X,Y)
+%
+nx	= size(X,1);
+ny	= size(Y,1);
+%
+D2 = (sum((X.^2), 2) * ones(1,ny)) + (ones(nx, 1) * sum((Y.^2),2)') - ...
+     2*X*Y';
+end
