faa42cf5d918b7b2d35cdbda011508b38df2b584
louisfaury <l.faury@hotmail.fr>
2017-04-15 10:07:09 +0200

    Adds code main header

---
+++ code/main.m
@@ -0,0 +1,13 @@
+%% Advanced Machine Learning Coding Project
+% \brief : Main script for the Relevance Vector Machine Project
+% \author : Gr?goire G.Montbrun
+%           Hadrien Hendrix
+%           Louis Faury
+% \date : 15/04/2017
+
+clear all;
+close all;
+clc
+
+
+%% TODO 
